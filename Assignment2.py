13b151fed3574f387a68a85639be25ee4c4ff023
Praveen Mahaulpatha <thejaka.mahaulpatha@gmail.com>
2022-07-27 14:30:54 -0400

    Add files via upload

--- Assignment2.py
+++ Assignment2.py
@@ -1,3 +1,6 @@
+import  numpy as np
+import math
+
 # Part-1) The simplest encryption algorithm! (mark 30%)
 
 def ceaser_cypher(message,shift):
@@ -30,25 +33,63 @@ ceaser_cypher("cheer",7)
 # Part-2) Binary Search Algorithm Implementation in Python (mark 30%)
 
 def binary_search(myList,num):
-    while True:
-        mid = int(len(myList)/2)
-        print(mid)
+    min = 0
+    max = len(myList)-1
+
+    while min<max:
+        mid = int(np.floor((min+max)/2))
+
         if num == myList[mid]:
-            print('Number Found')
             return True
         elif num < myList[mid]:
-            myList = myList[0:mid]
-        elif num > myList[mid]:
-            myList = myList[mid+1:len(myList)]
-            print("Okay")
+            max = mid -1
+        # elif num > myList[mid]:
+        #     min = mid+1
         else:
-            print('Number Not Found')
-            return False
-        # return True
+            min = mid + 1
+        #     return False
+    return False
 
 numberList = [1,2,3,4,5,6,7]
+print(binary_search(numberList,6))
+print(binary_search(numberList,27))
 
-binary_search(numberList,78)
+# Part-3) Formula Implementation (mark 20%)
 
+def estimate_pi():
+    k=0
+    sum=0
+    point = pow(10,-15)
 
-def binary_search(myList,num):
+    while True:
+        term = (math.factorial(4*k)*(1103+26390*k))/(pow(math.factorial(k),4)*pow(396,(4*k)))
+        k+=1
+        if term > point:
+            sum+=term
+        else:
+            break
+
+    pi = 9801/(2*np.sqrt(2)*sum)
+    return pi
+
+print(estimate_pi())
+
+# Part-4) Write the corresponding Python code for the following flowchart (mark 20%)
+
+def flowChart():
+    n = int(input("Please enter a Number: "))
+
+    x=0
+
+    while n != 0:
+
+        if n%2 ==0:
+            x = x+n
+        else:
+            x = x+n
+
+        n-=1
+
+    return x
+
+print(flowChart())
